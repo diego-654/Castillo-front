80283e6326194e7b491794470f65cf8c0f8e9be1
sergiovalerio <sergio.ickk@gmail.com>
2025-11-21 13:54:40 -0500

    current merge

--- src/app/features/interesados/presentation/pages/datos-obtenidos/datos-obtenidos.ts
+++ src/app/features/interesados/presentation/pages/datos-obtenidos/datos-obtenidos.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { SearchInputComponent } from '@shared/components/search-input/search-input.component';
 
 @Component({
   selector: 'app-datos-obtenidos',
-  imports: [],
+  imports: [SearchInputComponent],
   templateUrl: './datos-obtenidos.html',
   styleUrl: './datos-obtenidos.scss',
 })
 export default class DatosObtenidos {
+  searchInputCheck = signal('');
+  searchInputDrag = signal('');
 
+  filteredChecksColumns = computed(() => {});
+
+  filteredDragColumns = computed(() => {});
 }
